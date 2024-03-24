14c597477b83c455aa54f28238c27d7d66e30edc
Attraya Das <attrayaghoshdas@gmail.com>
2024-03-24 17:52:08 +0530

    Create Spring Data JPA Repository

---
+++ src/main/java/com/attraya/repository/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.attraya.repository;
+
+import com.attraya.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
