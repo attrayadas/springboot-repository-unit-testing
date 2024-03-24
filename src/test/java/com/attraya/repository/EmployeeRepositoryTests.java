e4f0b0459f50c4bea6351855662c9109cc0c609a
Attraya Das <attrayaghoshdas@gmail.com>
2024-03-24 18:11:58 +0530

    Unit Test for Get All Employees Operation

--- src/test/java/com/attraya/repository/EmployeeRepositoryTests.java
+++ src/test/java/com/attraya/repository/EmployeeRepositoryTests.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 @DataJpaTest
 public class EmployeeRepositoryTests {
 
@@ -28,5 +30,30 @@ public class EmployeeRepositoryTests {
         // then - verify the output
         assertThat(savedEmployee).isNotNull();
         assertThat(savedEmployee.getId()).isGreaterThan(0);
+    }
+
+    // Junit test for get all employee operation
+    @Test
+    public void givenEmployeeList_whenFindAll_thenReturnEmployeeList(){
+        // given - precondition or setup
+        Employee employee1 = Employee.builder()
+                .firstName("Attraya")
+                .lastName("Das")
+                .email("attraya@gmail.com")
+                .build();
+        Employee employee2 = Employee.builder()
+                .firstName("Sudhakar")
+                .lastName("S")
+                .email("sudhakar.s@gmail.com")
+                .build();
+        employeeRepository.save(employee1);
+        employeeRepository.save(employee2);
+
+        // when - action or the behavior that we are going to test
+        List<Employee> employeeList = employeeRepository.findAll();
+
+        // then - verify the output
+        assertThat(employeeList).isNotNull();
+        assertThat(employeeList.size()).isEqualTo(2);
     }
 }
